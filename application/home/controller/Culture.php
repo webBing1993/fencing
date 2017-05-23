15be0ce793116dbeb455378343f913b01205d0c0
yangmingzhe <15700004138@163.com>
2017-05-23 16:15:47 +0800

    文明创建修改bug

--- application/home/controller/Culture.php
+++ application/home/controller/Culture.php
@@ -183,7 +183,7 @@ class Culture extends Base{
                 'aid' => $id,
                 'status' => 0,
                 'type' => 14,
-                'aid' => $userId
+                'uid' => $userId
             );
             $msg = Like::where($map2)->find();
             if($msg) {
