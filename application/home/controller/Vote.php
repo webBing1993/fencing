6d967ff00f38bdb931ccbc3152e0977c0a71eb84
wzc <958364865@qq.com>
2017-07-19 10:06:02 +0800

    支部工作 修改

--- application/home/controller/Vote.php
+++ application/home/controller/Vote.php
@@ -248,10 +248,10 @@ class Vote extends Base{
             'publisher' => ['in',$depart],
         );
         $list = Db::table('pb_appraise')->where($map)->order('id desc')->limit($len,10)->select();
-        foreach($list as $value){
+        foreach($list as $key => $value){
             $Pic = Picture::where('id',$value['front_cover'])->find();
-            $value['front_cover'] = $Pic['path'];
-            $value['create_time'] = date('Y-m-d',$value['create_time']);
+            $list[$key]['front_cover'] = $Pic['path'];
+            $list[$key]['create_time'] = date('Y-m-d',$value['create_time']);
         }
         if ($list){
             return $this->success('加载成功','',$list);
