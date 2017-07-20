310e5df7d31fdc67108ad47eb7a3716173fea9fd
wzc <958364865@qq.com>
2017-07-20 09:54:00 +0800

    三会一课  修改

--- application/home/controller/Vote.php
+++ application/home/controller/Vote.php
@@ -464,12 +464,10 @@ class Vote extends Base{
     public function notes(){
         $this->checkRole();
         $userId = session('userId');
-        $a = array('1'=>'a','2'=>'b','3'=>'c','4'=>'d','5'=>'e','6'=>'f','7'=>'g','8'=>'h','9'=>'i','10'=>'j','11'=>'k','12'=>'l','13'=>'m','14'=>'n','15'=>'o',
-            '16'=>'p','17'=>'q','18'=>'r','19'=>'s','20'=>'t','21'=>'u','22'=>'v','23'=>'w','24'=>'x','25'=>'y','26'=>'z');
         $Work = new Work();
         if(IS_POST) {
             $data = input('post.');
-            $data['front_cover'] = array_rand($a,1);
+            $data['front_cover'] = mt_rand(1,15);
             $data['publisher'] = $userId;
             if (isset($data['images'])){
                 $data['images'] = json_encode($data['images']);
