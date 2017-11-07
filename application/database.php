0b4be3b244164166e253209f05cc66f100b00c19
laowang <958364865@qq.com>
2017-11-07 09:31:20 +0800

    框架结构

--- application/database.php
+++ application/database.php
@@ -18,11 +18,11 @@ return [
     // 服务器地址
     'hostname'    => '192.168.1.60',
     // 数据库名
-    'database'    => 'tzgxpb',
+    'database'    => 'rhpb',
     // 数据库用户名
-    'username'    => 'guest',
+    'username'    => 'root',
     // 数据库密码
-    'password'    => '123456',
+    'password'    => ' ',
     // 数据库连接端口
     'hostport'    => '3306',
     // 数据库连接参数
