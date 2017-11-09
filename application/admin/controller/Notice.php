3075bc9a4634c14da51b5a708a361006ed6c2131
Administrator <512331013@qq.com>
2017-11-09 18:30:50 +0800

    后台支部活动模块里面的会议纪要和固定活动两个小模块的增删改查四项

--- application/admin/controller/Notice.php
+++ application/admin/controller/Notice.php
@@ -122,4 +122,282 @@ class Notice extends Admin {
             return $this->error("删除失败");
         }
     }
+
+    //会议纪要
+    public function meeting(){
+
+            $map = array(
+                'type' => 3,
+                'status' => array('egt',1),
+            );
+            $list = $this->lists('Notice',$map);
+            int_to_string($list,array(
+                'status' => array(1=>"已发布"),
+                'recommend' => array(0 => "否" ,1 => "是")
+            ));
+            $this->assign('list',$list);
+            return $this->fetch();
+
+    }
+    //会议纪要添加
+    public function meetingadd(){
+        if(IS_POST) {
+            $data = input('post.');
+            //dump($data);
+            //exit();
+            $result = $this->validate($data,'Notice');  // 验证  数据
+            $data['create_user'] = $_SESSION['think']['user_auth']['id'];
+            if (true !== $result) {
+                return $this->error($result);
+            }else{
+                $noticeModel = new NoticeModel();
+                $data['start_time'] = strtotime($data['start_time']);
+                $data['end_time'] = strtotime($data['end_time']);
+                if (!empty($data['start_time']) && empty($data['end_time'])){
+                    return $this->error('请添加结束时间');
+                }
+                if (empty($data['start_time']) && !empty($data['end_time'])){
+                    return $this->error('请添加开始时间');
+                }
+                if (!empty($data['start_time']) && !empty($data['end_time']) && $data['end_time'] <= $data['start_time']){
+                    return $this->error('结束时间有错误');
+                }
+                $res = $noticeModel->save($data);
+                if ($res){
+                    return $this->success("会议纪要添加成功",Url('Notice/meeting'));
+                }else{
+                    return $this->error($noticeModel->getError());
+                }
+            }
+        }else {
+            return $this->fetch();
+        }
+    }
+      //会议纪要删除
+    public function meetingdel(){
+        $id = input('id');
+        if (empty($id)){
+            return $this->error('系统参数错误');
+        }
+        $map['status'] = "-1";
+        $info = NoticeModel::where('id',$id)->update($map);
+        if($info) {
+            return $this->success("删除成功");
+        }else{
+            return $this->error("删除失败");
+        }
+    }
+
+    /**
+     * 会议纪要 修改
+     */
+    public function meetingedit(){
+        if(IS_POST) {
+            $data = input('post.');
+            $result = $this->validate($data,'Notice');  // 验证  数据
+            $data['create_user'] = $_SESSION['think']['user_auth']['id'];
+            if (true !== $result) {
+                return $this->error($result);
+            }else{
+                $noticeModel = new NoticeModel();
+                if (!empty($data['start_time']) && empty($data['end_time'])){
+                    return $this->error('请添加结束时间');
+                }
+                if (empty($data['start_time']) && !empty($data['end_time'])){
+                    return $this->error('请添加开始时间');
+                }
+                if (!empty($data['start_time']) && !empty($data['end_time']) && $data['end_time'] <= $data['start_time']){
+                    return $this->error('结束时间有错误');
+                }
+                $data['start_time'] = strtotime($data['start_time']);
+                $data['end_time'] = strtotime($data['end_time']);
+                $res = $noticeModel->save($data,['id'=>$data['id']]);
+                if ($res){
+                    return $this->success("修改纪要成功",Url('Notice/meeting'));
+                }else{
+                    return $this->get_update_error_msg($noticeModel->getError());
+                }
+            }
+        }else{
+            $id = input('id');
+            $msg = NoticeModel::get($id);
+            $this->assign('msg',$msg);
+            return $this->fetch();
+        }
+    }
+    /**
+     * 活动展示
+     */
+    public function show(){
+        $map = array(
+            'type' => 2,
+            'status' => array('egt',1),
+        );
+        $list = $this->lists('Notice',$map);
+        int_to_string($list,array(
+            'status' => array(1=>"已发布"),
+            'recommend' => array(0 => "否" ,1 => "是")
+        ));
+        $this->assign('list',$list);
+        return $this->fetch();
+    }
+
+    /**
+     * 活动展示 添加
+     */
+    public function showadd(){
+        if(IS_POST) {
+            $data = input('post.');
+            $result = $this->validate($data,'Notice');  // 验证  数据
+            $data['create_user'] = $_SESSION['think']['user_auth']['id'];
+            if (true !== $result) {
+                return $this->error($result);
+            }else{
+                $noticeModel = new NoticeModel();
+                $data['start_time'] = strtotime($data['start_time']);
+                $data['end_time'] = strtotime($data['end_time']);
+                if (!empty($data['start_time']) && empty($data['end_time'])){
+                    return $this->error('请添加结束时间');
+                }
+                if (empty($data['start_time']) && !empty($data['end_time'])){
+                    return $this->error('请添加开始时间');
+                }
+                if (!empty($data['start_time']) && !empty($data['end_time']) && $data['end_time'] <= $data['start_time']){
+                    return $this->error('结束时间有错误');
+                }
+                $res = $noticeModel->save($data);
+                if ($res){
+                    return $this->success("新增活动展示成功",Url('Notice/show'));
+                }else{
+                    return $this->error($noticeModel->getError());
+                }
+            }
+        }else {
+            return $this->fetch();
+        }
+    }
+
+    //活动展示删除
+    public function showdel(){
+        $id = input('id');
+        if (empty($id)){
+            return $this->error('系统参数错误');
+        }
+        $map['status'] = "-1";
+        $info = NoticeModel::where('id',$id)->update($map);
+        if($info) {
+            return $this->success("删除成功");
+        }else{
+            return $this->error("删除失败");
+        }
+    }
+
+    /**
+     * 活动展示 修改
+     */
+    public function showedit(){
+        if(IS_POST) {
+            $data = input('post.');
+            $result = $this->validate($data,'Notice');  // 验证  数据
+            $data['create_user'] = $_SESSION['think']['user_auth']['id'];
+            if (true !== $result) {
+                return $this->error($result);
+            }else{
+                $noticeModel = new NoticeModel();
+                if (!empty($data['start_time']) && empty($data['end_time'])){
+                    return $this->error('请添加结束时间');
+                }
+                if (empty($data['start_time']) && !empty($data['end_time'])){
+                    return $this->error('请添加开始时间');
+                }
+                if (!empty($data['start_time']) && !empty($data['end_time']) && $data['end_time'] <= $data['start_time']){
+                    return $this->error('结束时间有错误');
+                }
+                $data['start_time'] = strtotime($data['start_time']);
+                $data['end_time'] = strtotime($data['end_time']);
+                $res = $noticeModel->save($data,['id'=>$data['id']]);
+                if ($res){
+                    return $this->success("修改活动成功",Url('Notice/show'));
+                }else{
+                    return $this->get_update_error_msg($noticeModel->getError());
+                }
+            }
+        }else{
+            $id = input('id');
+            $msg = NoticeModel::get($id);
+            $this->assign('msg',$msg);
+            return $this->fetch();
+        }
+    }
+
+    /**
+     * 固定活动
+     */
+    public function activity(){
+        $map = array(
+            'type' => 4,
+            'status' => array('egt',1),
+        );
+        $list = $this->lists('Notice',$map);
+        int_to_string($list,array(
+            'status' => array(1=>"已发布"),
+            'recommend' => array(0 => "否" ,1 => "是")
+        ));
+        $this->assign('list',$list);
+        return $this->fetch();
+    }
+
+    /**
+     * 固定活动 添加
+     */
+    public function activityadd(){
+        if(IS_POST) {
+            $data = input('post.');
+            $result = $this->validate($data,'Activity');  // 验证  数据
+            $data['create_user'] = $_SESSION['think']['user_auth']['id'];
+            if (true !== $result) {
+                return $this->error($result);
+            }else{
+                $noticeModel = new NoticeModel();
+
+                $res = $noticeModel->save($data);
+                if ($res){
+                    return $this->success("新增活动成功",Url('Notice/activity'));
+                }else{
+                    return $this->error($noticeModel->getError());
+                }
+            }
+        }else {
+            return $this->fetch();
+        }
+    }
+
+    /**
+     * 活动安排 修改
+     */
+    public function activityedit(){
+        if(IS_POST) {
+            $data = input('post.');
+            $result = $this->validate($data,'Activity');  // 验证  数据
+            $data['create_user'] = $_SESSION['think']['user_auth']['id'];
+            if (true !== $result) {
+                return $this->error($result);
+            }else{
+                $noticeModel = new NoticeModel();
+                $res = $noticeModel->save($data,['id'=>$data['id']]);
+                if ($res){
+                    return $this->success("修改活动成功",Url('Notice/activity'));
+                }else{
+                    return $this->get_update_error_msg($noticeModel->getError());
+                }
+            }
+        }else{
+            $id = input('id');
+            $msg = NoticeModel::get($id);
+            $this->assign('msg',$msg);
+            return $this->fetch();
+        }
+    }
+    
+
 }
