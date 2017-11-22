b025cfac83502488861e0c35d71d762317bb4d5c
laowang <958364865@qq.com>
2017-11-22 10:25:11 +0800

    签到管理  修改

--- application/home/controller/Signin.php
+++ application/home/controller/Signin.php
@@ -42,26 +42,27 @@ class Signin extends Base {
         }else{
             $Wechat = WechatUser::where(['userid'=>$userid])->find();
             if($Wechat){
-                $data = array(
-                    'sign_id' => $id,
-                    'userid' =>$userid,
-                    'score' => 0,
-                    'status' =>0,
-                    'create_time' => time()
-                );
                 $work = new Work();
                 $Apply = new Apply();
                 $type = $work->where('id',$id)->value('type');  // 1 三会一课  2 志愿之家
                 if ($type == 1){
+                    $data = array(
+                        'sign_id' => $id,
+                        'userid' =>$userid,
+                        'score' => 0,
+                        'status' =>0,
+                        'type' => $type,
+                        'create_time' => time()
+                    );
                     $res = $Apply->save($data);
                 }else{
                     $mouth = date('m',time());
                     $score = $Apply->where(['userid' => $userid])->whereTime('create_time','d')->sum('score');
                     if ($score < 2){
                         WechatUser::where('userid',$userid)->setInc('volunteer_score');
-                        $res = $Apply->save(['sign_id' => $id,'userid' => $userid,'score' => 1,'status' => 0,'mouth' => $mouth,'create_time' => time()]);
+                        $res = $Apply->save(['sign_id' => $id,'userid' => $userid,'score' => 1,'status' => 0,'type' =>$type,'mouth' => $mouth,'create_time' => time()]);
                     }else{
-                        $res = $Apply->save(['sign_id' => $id,'userid' => $userid,'score' => 0,'mouth' => $mouth,'status' => 0,'create_time' => time()]);
+                        $res = $Apply->save(['sign_id' => $id,'userid' => $userid,'score' => 0,'mouth' => $mouth,'type' =>$type,'status' => 0,'create_time' => time()]);
                     }
                 }
                 if($res){
