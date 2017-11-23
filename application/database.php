ea41b524cb1d47af1d11c0b37a4d0382ac5041d5
Administrator <fangguozhen@gmail.com>
2017-11-23 21:04:27 +0800

    已审核列表加载

--- application/database.php
+++ application/database.php
@@ -16,13 +16,13 @@ return [
     // 数据库连接DSN配置
     'dsn'         => '',
     // 服务器地址
-    'hostname'    => '192.168.1.60',
+    'hostname'    => '192.168.1.56',
     // 数据库名
     'database'    => 'rhpb',
     // 数据库用户名
-    'username'    => 'root',
+    'username'    => 'guest',
     // 数据库密码
-    'password'    => ' ',
+    'password'    => '',
     // 数据库连接端口
     'hostport'    => '3306',
     // 数据库连接参数
