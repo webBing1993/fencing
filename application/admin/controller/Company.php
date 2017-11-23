60bdbae646f549ad15b68bc7fcd25214a6980f60
Administrator <512331013@qq.com>
2017-11-23 15:50:27 +0800

    后台风采展修改置顶字段

--- application/admin/controller/Company.php
+++ application/admin/controller/Company.php
@@ -155,7 +155,7 @@ class Company extends Admin
         $list = $this->lists('Companys', $map);
         int_to_string($list, array(
             'status' => array( 0=>"待审核",1=>"已发布",2=>"审核未通过"),
-            'istop'=>array(0=>"否",1=>"是"),
+            'recommend'=>array(0=>"否",1=>"是"),
         ));
        
         $this->assign('list', $list);
