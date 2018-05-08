3f35314829467f02ab465263bc2730b69779ef5a
laowang <958364865@qq.com>
2018-05-08 15:52:01 +0800

    workman

---
+++ public/server.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laowang
+ * Date: 2018/5/8
+ * Time: 上午10:54
+ */
+define('APP_PATH', __DIR__ . '/../application/');
+define('BIND_MODULE','push/Worker');
+// 加载框架引导文件
+require __DIR__ . '/../thinkphp/start.php';
