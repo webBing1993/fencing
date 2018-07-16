fa64d025d57716d1d6b45472353b52872a3b0381
Administrator <dairenjie000@163.com>
2018-07-16 17:02:47 +0800

    击剑风云

--- application/home/controller/Association.php
+++ application/home/controller/Association.php
@@ -14,4 +14,25 @@ class Association  extends Base
     public function index(){
         return $this->fetch();
     }
+    public function news(){
+        return $this->fetch();
+    }
+    public function daquan(){
+        return $this->fetch();
+    }
+    public function style(){
+        return $this->fetch();
+    }
+    public function fencing(){
+        return $this->fetch();
+    }
+    public function jjdetail(){
+        return $this->fetch();
+    }
+    public function newsmore(){
+        return $this->fetch();
+    }
+    public function newsdetail(){
+        return $this->fetch();
+    }
 }
