f5b6c273a6199d79303c983ee87c8720dea85d31
Administrator <512331013@qq.com>
2018-07-20 09:11:53 +0800

    后台场馆模块 经纬度添加

--- application/admin/validate/Venue.php
+++ application/admin/validate/Venue.php
@@ -12,22 +12,26 @@ use think\Validate;
 
 class Venue extends Validate {
     protected $rule = [
-        'front_cover'  =>  'require',
         'title' =>  'require',
         'address' =>  'require',
+        'lng' =>  'require',
+        'lat' =>  'require',
         'tel' =>  'require',
         'time' =>  'require',
         'content'  =>  'require',
+        'front_cover'  =>  'require',
 //        'publisher'  =>  'require',
     ];
 
     protected $message = [
-        'front_cover.require'  =>  '请添加封面图片！',
         'title.require' =>  '请添加场馆名称！',
         'address.require' =>  '请添加详细地址！',
+        'lng.require' =>  '请添加经度！',
+        'lat.require' =>  '请添加纬度！',
         'tel.require' =>  '请添加联系电话！',
         'time.require' =>  '请添加开放时间！',
         'content.require'  =>  '请填写内容！',
+        'front_cover.require'  =>  '请添加场馆图片！',
 //        'publisher.require'  =>  '请填写发布人名称！',
     ];
 
