39c1ebdda77e4d0f162a9de5fe0ab20d0d92f766
Administrator <512331013@qq.com>
2018-07-24 15:21:19 +0800

    商城

--- application/home/controller/Mall.php
+++ application/home/controller/Mall.php
@@ -12,6 +12,7 @@ use app\home\model\Shop;
 use app\home\model\MallOne;
 use app\home\model\MallTwo;
 use app\home\model\ShopRecord;
+use app\home\model\ShopOrder;
 
 /*
  * 商城
@@ -29,6 +30,9 @@ class Mall  extends Base
         //类别
         $type2 = MallTwo::where('status',0)->order('id desc')->select();
         $this->assign('type2',$type2);
+        //历史搜索记录
+        $ss = ShopRecord::where('status',0)->where('create_user',session('userId'))->order('id desc')->limit(4)->select();
+        $this->assign('ss',$ss);
 
         return $this->fetch();
     }
@@ -43,6 +47,24 @@ class Mall  extends Base
             return $this->error("搜索失败");
         }
     }
+    // 筛选
+    public function screen(){
+        $type1 = input('type1');
+        $type2 = input('type2');
+        if(!empty($type1)){
+            $map['type1'] = $type1;
+        }
+        if(!empty($type2)){
+            $map['type2'] = $type2;
+        }
+        $map['status'] = 0;
+        $data = Shop::where($map)->order('id desc')->limit(12)->select();
+        if($data) {
+            return $this->success("筛选成功",'',$data);
+        }else{
+            return $this->error("筛选失败");
+        }
+    }
 
     // 商城详情页
     public function detail(){
@@ -55,16 +77,49 @@ class Mall  extends Base
             $data = Shop::where('id',$id)->find();
         }else{
             $data = Shop::where('id',$id)->find();
+            $li['ssid'] = $data['id'];
             $li['title'] = $data['title'];
+            $li['create_user'] = session('userId');
             $li['create_time'] = time();
             ShopRecord::create($li);
         }
+        //剑种赋值
+        $data['type1'] = MallOne::where('id',$data['type1'])->value('title');
+        //类别赋值
+        $data['type2'] = MallTwo::where('id',$data['type2'])->value('title');
         $this->assign('data',$data);
 
         return $this->fetch();
     }
 
-    // 商城订单结算页
+    //订单生成(未付款)
+    public function ordering(){
+        $id = input('id');
+        $num = input('num');
+        $shop = Shop::where('id',$id)->where('status',0)->find();
+        if(empty($shop)){
+            return $this->error("该商品已下架");
+        }elseif($num < 1){
+            return $this->error("请核对下单商品的数量");
+        }else{
+            $data['sid'] = $id;
+            $data['num'] = $num;
+            $data['price'] = $shop['price'];
+            $data['create_time'] = time();
+            $data['create_user'] = session('userId');
+            $data['total'] = $num * $shop['price'];
+            $ShopOrderModel = new ShopOrder();
+            $info = $ShopOrderModel->save($data);
+            if($info) {
+                $oid = $ShopOrderModel->id;
+                return $this->success("订单生成成功",'',$oid);
+            }else{
+                return $this->error("订单生成失败");
+            }
+        }
+    }
+
+    // 商城订单结算页()
     public function order(){
 
         return $this->fetch();
---
+++ application/home/model/ShopOrder.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 王泽锋
+ * Date: 2018/07/24
+ * Time: 14:04
+ */
+
+namespace app\home\model;
+
+use think\Model;
+
+class ShopOrder extends Model {
+
+}
