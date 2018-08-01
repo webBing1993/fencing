4eb201e3c44086df03a1e4a8c8e6af8ef4dc657c
Administrator <512331013@qq.com>
2018-08-01 13:59:07 +0800

    个人中心 请假申请

--- application/home/controller/User.php
+++ application/home/controller/User.php
@@ -9,6 +9,7 @@
 namespace app\home\controller;
 
 
+use app\home\model\Apply;
 use app\home\model\WechatUser;
 
 class User extends Base
@@ -45,10 +46,29 @@ class User extends Base
     public function play(){
         return $this->fetch();
     }
+
+    //个人中心  请假申请(列表首页)
     public function leave(){
+        $userId = session('userId');
+        $left = Apply::where('create_user',$userId)->where('status',0)->order('id desc')->limit(6)->select();//待审核
+        $right = Apply::where('create_user',$userId)->where('status','>',0)->order('id desc')->limit(6)->select();//已审核
+        $this->assign('left',$left);
+        $this->assign('right',$right);
+
         return $this->fetch();
     }
+
     public function leavedetail(){
+        $Id = input('id');
+        $data = Apply::where('id',$Id)->find();
+        $user = WechatUser::where('mobile',$data['create_user'])->find();
+        $data['create_user'] = $user['name'];
+        if(!empty($data['front_cover'])){
+            $data['front_cover'] = json_decode($data['front_cover']);
+        }
+        $this->assign('data',$data);
+//        dump($data);exit();
+
         return $this->fetch();
     }
 
@@ -60,13 +80,20 @@ class User extends Base
 
     //个人中心  请假申请 提交数据库
     public function apply(){
-        $data = input('post');
-        dump($data);exit;
+        $data = input('post.');
+        if(!empty($data['front_cover'])){
+            $data['front_cover'] = json_encode($data['front_cover']);
+        }
+        $data['create_user'] = session('userId');
+        $data['create_time'] = time();
+        $info = Apply::create($data);
 
-
-        return $this->fetch();
+        if($info) {
+            return $this->success("提交成功");
+        }else{
+            return $this->error("提交失败");
+        }
     }
-
 
     public function reite(){
         return $this->fetch();
---
+++ application/home/model/Apply.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 王泽锋
+ * Date: 2018/08/01
+ * Time: 10:20
+ */
+
+namespace app\home\model;
+
+use think\Model;
+
+class Apply extends Model {
+
+}
