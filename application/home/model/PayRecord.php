513c8988057d78f345e5600bbbd416df46dedaba
ben <750405122@qq.com>
2018-08-09 15:46:17 +0800

    支付接口

--- application/home/model/PayRecord.php
+++ application/home/model/PayRecord.php
@@ -10,5 +10,14 @@ namespace app\home\model;
 use think\Model;
 
 class PayRecord extends Model {
-
+    const TYPE_1 = 1;
+    const TYPE_2 = 2;
+    const TYPE_3 = 3;
+    const TYPE_4 = 4;
+    const TYPE_ARRAY = [
+        self::TYPE_1 => 'course_apply',//课程报名
+        self::TYPE_2 => 'competition_apply',//比赛报名
+        self::TYPE_3 => 3,//升级会员
+        self::TYPE_4 => 'shop_order',//商城购买
+    ];
 }
