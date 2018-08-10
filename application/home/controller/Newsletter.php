f231ecd351cbca3abece73277ef9ed3bc6c57c08
Administrator <512331013@qq.com>
2018-08-10 17:19:35 +0800

    通讯录

--- application/home/controller/Newsletter.php
+++ application/home/controller/Newsletter.php
@@ -12,11 +12,33 @@ namespace app\home\controller;
 /*
  * 通讯名录
  * */
+use app\home\model\WechatDepartment;
+
 class Newsletter  extends Base
 {
     // 通讯名录首页
     public function index(){
+        $one = WechatDepartment::where('parentid',1)->select();
+        foreach($one as $k=>$v){
+            $two = WechatDepartment::where('parentid',$v['id'])->select();
+            if(!empty($two)){
+                foreach($two as $k2=>$v2){
+                    $three = WechatDepartment::where('parentid',$v2['id'])->select();
+                    if(!empty($three)){
+                        $two[$k2]['three'] = $three;
+                        $two[$k2]['threepd'] = 1;
+                    }else{
+                        $two[$k2]['threepd'] = 0;
+                    }
+                }
+                $one[$k]['twopd'] = 1;
+                $one[$k]['two'] = $two;
 
+            }else{
+                $one[$k]['twopd'] = 0;
+            }
+        }
+        $this->assign('one',$one);
 
         return $this->fetch();
     }
