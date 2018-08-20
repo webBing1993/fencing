4da5f4a2bd1a47e4b9a5890b07437fadbd12cc41
Administrator <512331013@qq.com>
2018-08-20 11:32:37 +0800

    比赛模块的推送

--- application/admin/controller/Competition.php
+++ application/admin/controller/Competition.php
@@ -60,6 +60,11 @@ class Competition extends Admin {
             $competitionModel = new CompetitionModel();
             $info = $competitionModel->validate('Competition')->save($data);
             if($info) {
+                if($data['push'] == 1){
+                    //推送
+                    $nid=$competitionModel->id;
+                    $this->pushto($nid);
+                }
                 return $this->success("添加成功",Url('Competition/index'));
             }else{
                 return $this->error($competitionModel->getError());
@@ -83,6 +88,11 @@ class Competition extends Admin {
             $competitionModel = new CompetitionModel();
             $info = $competitionModel->validate('Competition')->save($data,['id'=>input('id')]);
             if($info){
+                if($data['push'] == 1){
+                    //推送
+                    $nid=$competitionModel->id;
+                    $this->pushto($nid);
+                }
                 return $this->success("修改成功",Url("Competition/index"));
             }else{
                 return $this->get_update_error_msg($competitionModel->getError());
@@ -129,4 +139,102 @@ class Competition extends Admin {
             return $this->error('批量删除失败');
         }
     }
+
+    /**
+     * 首页推送按钮
+     */
+    public function push(){
+        $nid = input('id');
+        //推送
+        $info = $this->pushto2($nid);
+
+        if($info) {
+            return $this->success("推送成功");
+        }else{
+            return $this->error("推送失败");
+        }
+    }
+
+
+    //推送给固定人员预览
+    public function pushto($nid){
+        $httpUrl = config('http_url');
+//                $httpUrl="http://".$_SERVER['HTTP_HOST'];
+        $arr1 = $nid;    //主图文id
+        //主图文信息
+        $focus1 = CompetitionModel::where('id',$arr1)->find();
+        $title1 = $focus1['title'];
+        $str1 = strip_tags($focus1['content']);
+        $des1 = mb_substr($str1,0,100);
+        $content1 = str_replace("&nbsp;","",$des1);  //空格符替换成空
+        $pre1 = "【击剑比赛】";
+        $url1 = $httpUrl."/home/association/gamedetail/id/".$focus1['id'].".html";
+        $img1 = Picture::get($focus1['front_cover']);
+        $path1 = $httpUrl.$img1['path'];
+        $send['articles'][0] = array(
+            "title" => $pre1.$title1,
+            "description" => $content1,
+            "url" => $url1,
+            "picurl" => $path1,
+        );
+        //发送给企业号
+        $Wechat = new TPQYWechat(Config::get('party'));
+        $touser = config('totestuser');
+        $newsConf = config('party');
+        $message = array(
+            "touser" => $touser,
+            "msgtype" => 'news',
+            "agentid" => $newsConf['agentid'],
+            "news" => $send,
+            "safe" => "0"
+        );
+        $msg = $Wechat->sendMessage($message);
+    }
+
+    //推送给所有人员
+    public function pushto2($nid){
+        $httpUrl = config('http_url');
+//                $httpUrl="http://".$_SERVER['HTTP_HOST'];
+        $arr1 = $nid;    //主图文id
+        $update['status'] = '1';
+        CompetitionModel::where(['id' => $arr1])->update($update); // 更新推送后的状态
+        //主图文信息
+        $focus1 = CompetitionModel::where('id', $arr1)->find();
+        $title1 = $focus1['title'];
+        $str1 = strip_tags($focus1['content']);
+        $des1 = mb_substr($str1, 0, 100);
+        $content1 = str_replace("&nbsp;", "", $des1);  //空格符替换成空
+        $pre1 = "【击剑比赛】";
+        $url1 = $httpUrl."/home/association/gamedetail/id/".$focus1['id'].".html";
+        $img1 = Picture::get($focus1['front_cover']);
+        $path1 = $httpUrl . $img1['path'];
+        $send['articles'][0] = array(
+            "title" => $pre1 . $title1,
+            "description" => $content1,
+            "url" => $url1,
+            "picurl" => $path1,
+        );
+        //发送给企业号
+        $Wechat = new TPQYWechat(Config::get('party'));
+        $touser = config('touser');
+        $newsConf = config('party');
+        //部门推送
+        if ($focus1['push'] == 1) {
+            $message = array(
+                "touser" => $touser,
+                "msgtype" => 'news',
+                "agentid" => $newsConf['agentid'],
+                "news" => $send,
+                "safe" => "0"
+            );
+            $msg = $Wechat->sendMessage($message);
+
+            if($msg){
+                return $this->success("推送成功",'',$msg);
+            }else{
+                return $this->error("推送失败");
+            }
+        }
+    }
+
 }
