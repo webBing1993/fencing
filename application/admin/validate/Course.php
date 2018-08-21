34084e20dadcbaab2772f4c03d2529150704566e
ben <750405122@qq.com>
2018-08-21 22:27:52 +0800

    备注

--- application/admin/validate/Course.php
+++ application/admin/validate/Course.php
@@ -10,22 +10,18 @@ namespace app\admin\validate;
 
 use think\Validate;
 
-class VenueCourse extends Validate {
+class Course extends Validate {
     protected $rule = [
-        'front_cover'  =>  'require',
         'type'  =>  'require',
         'course_name' =>  'require',
         'start_time'  =>  'require',
         'end_time'  =>  'require',
         'num'  =>  'require|number',
         'course_time'  =>  'require',
-        'content'  =>  'require',
 //        'remark'  =>  'require',
-        'price'  =>  'require',
     ];
 
     protected $message = [
-        'front_cover.require'  =>  '请添加封面图片！',
         'type.require'  =>  '请选择课程类型！',
         'course_name.require' =>  '请添加课程名称！',
         'start_time.require'  =>  '请选择课程开始时间！',
@@ -33,9 +29,7 @@ class VenueCourse extends Validate {
         'num.require'  =>  '请填写总课时数！',
         'num.number'  =>  '总课时数必须是数字！',
         'course_time.require'  =>  '请填写上课时间！',
-        'content.require'  =>  '请填写课程内容！',
 //        'remark.require'  =>  '请填写备注！',
-        'price.require'  =>  '请填写价格！',
     ];
 
 }
