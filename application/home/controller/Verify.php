318b1601f306a8c27d819932005cdd5e68bbaeb5
ben <750405122@qq.com>
2018-08-24 11:04:03 +0800

    签到二维码字符串转小写

--- application/home/controller/Verify.php
+++ application/home/controller/Verify.php
@@ -59,7 +59,7 @@ class Verify extends Controller{
                 $weObj = new TPWechat(config('weixinpay'));
                 $toOpenId = $weObj->convertToOpenId(['userid' => $result['UserId']]);
                 if($toOpenId){
-                    $user['openid'] = substr($toOpenId['openid'], 20);
+                    $user['openid'] = strtolower(substr($toOpenId['openid'], 20));
                 }
             }
 
