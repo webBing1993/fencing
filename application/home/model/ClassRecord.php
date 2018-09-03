d3ed1808775a6fea22f832d654a84a6148b9fcc5
ben <750405122@qq.com>
2018-09-03 14:22:26 +0800

    去重

--- application/home/model/ClassRecord.php
+++ application/home/model/ClassRecord.php
@@ -13,7 +13,7 @@ class ClassRecord extends Model {
 
     public static function getCoach($start, $end, $userId){
         $class_id = ClassRecord::where(['start_time' => ['>=', $start], 'end_time' => ['<=', $end], 'userid' => $userId, 'status' => 0])->column('class_id');
-        $userid_arr = ClassRecord::where(['class_id' => ['in', $class_id], 'member_type' => ['<>', 2], 'status' => 0])->column('userid');
+        $userid_arr = ClassRecord::where(['class_id' => ['in', $class_id], 'member_type' => ['<>', 2], 'status' => 0])->group('userid')->column('userid');
         return $userid_arr;
     }
 }
